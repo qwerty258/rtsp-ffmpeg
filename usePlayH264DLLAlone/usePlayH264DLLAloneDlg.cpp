302f7e6bbab1c14e897340396df560d58ccbb2cf
qwerty258 <zyf26256@gmail.com>
2015-08-19 16:06:20 +0800

    auto format source code

--- usePlayH264DLLAlone/usePlayH264DLLAloneDlg.cpp
+++ usePlayH264DLLAlone/usePlayH264DLLAloneDlg.cpp
@@ -16,20 +16,19 @@
 
 
 
-CusePlayH264DLLAloneDlg::CusePlayH264DLLAloneDlg(CWnd* pParent /*=NULL*/)
-	: CDialogEx(CusePlayH264DLLAloneDlg::IDD, pParent)
+CusePlayH264DLLAloneDlg::CusePlayH264DLLAloneDlg(CWnd* pParent /*=NULL*/): CDialogEx(CusePlayH264DLLAloneDlg::IDD, pParent)
 {
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
 void CusePlayH264DLLAloneDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+    CDialogEx::DoDataExchange(pDX);
 }
 
 BEGIN_MESSAGE_MAP(CusePlayH264DLLAloneDlg, CDialogEx)
-	ON_WM_PAINT()
-	ON_WM_QUERYDRAGICON()
+    ON_WM_PAINT()
+    ON_WM_QUERYDRAGICON()
 END_MESSAGE_MAP()
 
 
@@ -37,16 +36,16 @@ END_MESSAGE_MAP()
 
 BOOL CusePlayH264DLLAloneDlg::OnInitDialog()
 {
-	CDialogEx::OnInitDialog();
+    CDialogEx::OnInitDialog();
 
-	// Set the icon for this dialog.  The framework does this automatically
-	//  when the application's main window is not a dialog
-	SetIcon(m_hIcon, TRUE);			// Set big icon
-	SetIcon(m_hIcon, FALSE);		// Set small icon
+    // Set the icon for this dialog.  The framework does this automatically
+    //  when the application's main window is not a dialog
+    SetIcon(m_hIcon, TRUE);			// Set big icon
+    SetIcon(m_hIcon, FALSE);		// Set small icon
 
-	// TODO: Add extra initialization here
+    // TODO: Add extra initialization here
 
-	return TRUE;  // return TRUE  unless you set the focus to a control
+    return TRUE;  // return TRUE  unless you set the focus to a control
 }
 
 // If you add a minimize button to your dialog, you will need the code below
@@ -55,33 +54,33 @@ BOOL CusePlayH264DLLAloneDlg::OnInitDialog()
 
 void CusePlayH264DLLAloneDlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // device context for painting
+    if(IsIconic())
+    {
+        CPaintDC dc(this); // device context for painting
 
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+        SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// Center icon in client rectangle
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
+        // Center icon in client rectangle
+        int cxIcon = GetSystemMetrics(SM_CXICON);
+        int cyIcon = GetSystemMetrics(SM_CYICON);
+        CRect rect;
+        GetClientRect(&rect);
+        int x = (rect.Width() - cxIcon + 1) / 2;
+        int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// Draw the icon
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialogEx::OnPaint();
-	}
+        // Draw the icon
+        dc.DrawIcon(x, y, m_hIcon);
+    }
+    else
+    {
+        CDialogEx::OnPaint();
+    }
 }
 
 // The system calls this function to obtain the cursor to display while the user drags
 //  the minimized window.
 HCURSOR CusePlayH264DLLAloneDlg::OnQueryDragIcon()
 {
-	return static_cast<HCURSOR>(m_hIcon);
+    return static_cast<HCURSOR>(m_hIcon);
 }
 
--- usePlayH264DLLAlone/usePlayH264DLLAloneDlg.h
+++ usePlayH264DLLAlone/usePlayH264DLLAloneDlg.h
@@ -8,24 +8,27 @@
 // CusePlayH264DLLAloneDlg dialog
 class CusePlayH264DLLAloneDlg : public CDialogEx
 {
-// Construction
+    // Construction
 public:
-	CusePlayH264DLLAloneDlg(CWnd* pParent = NULL);	// standard constructor
+    CusePlayH264DLLAloneDlg(CWnd* pParent = NULL);	// standard constructor
 
-// Dialog Data
-	enum { IDD = IDD_USEPLAYH264DLLALONE_DIALOG };
+    // Dialog Data
+    enum
+    {
+        IDD = IDD_USEPLAYH264DLLALONE_DIALOG
+    };
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
-
-
-// Implementation
 protected:
-	HICON m_hIcon;
+    virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 
-	// Generated message map functions
-	virtual BOOL OnInitDialog();
-	afx_msg void OnPaint();
-	afx_msg HCURSOR OnQueryDragIcon();
-	DECLARE_MESSAGE_MAP()
+
+    // Implementation
+protected:
+    HICON m_hIcon;
+
+    // Generated message map functions
+    virtual BOOL OnInitDialog();
+    afx_msg void OnPaint();
+    afx_msg HCURSOR OnQueryDragIcon();
+    DECLARE_MESSAGE_MAP()
 };
