a8c9b27de9bfbe33146cba135cebd28021e3767d
qwerty258 <zyf26256@gmail.com>
2015-12-07 18:27:09 +0800

    add install uninstall

---
+++ install.bat
@@ -0,0 +1,5 @@
+copy ".\Release\RTSPFFmpeg.dll"         "%SystemRoot%\SysWOW64"
+copy ".\Release\PlayH264DLL.dll"        "%SystemRoot%\SysWOW64"
+
+copy ".\x64\Release\RTSPFFmpeg.dll"     "%SystemRoot%\System32"
+copy ".\x64\Release\PlayH264DLL.dll"    "%SystemRoot%\System32"
---
+++ uninstall.bat
@@ -0,0 +1,5 @@
+del "%SystemRoot%\SysWOW64\RTSPFFmpeg.dll"
+del "%SystemRoot%\SysWOW64\PlayH264DLL.dll"
+
+del "%SystemRoot%\System32\RTSPFFmpeg.dll"
+del "%SystemRoot%\System32\PlayH264DLL.dll"
